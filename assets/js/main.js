d773780c7f8fe539baf319628e31530c04dee1b4
Abu Hasan Rumi <30332740+abuhasanrumi@users.noreply.github.com>
2019-11-04 23:42:58 +0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,36 @@
-function openNav(){document.getElementById("myNav").style.width="100%"}function closeNav(){document.getElementById("myNav").style.width="0%"}$("a").on("click",function(t){if(""!==this.hash){t.preventDefault();var o=this.hash;$("html, body").animate({scrollTop:$(o).offset().top},800,function(){window.location.hash=o})}}),AOS.init(),$('[data-spy="scroll"]').each(function(){$(this).scrollspy("refresh")}),$(window).scroll(function(){$(this).scrollTop()>0?$(".header").addClass("sticky"):$(".header").removeClass("sticky")}),$(function(){$(".portfolio-items").isotope({itemSelector:".port-img",layoutMode:"fitRows"}),$(".portfolio-menu ul li").click(function(){$(".portfolio-menu ul li").removeClass("active"),$(this).addClass("active");var t=$(this).attr("data-filter");return $(".portfolio-items").isotope({filter:t}),!1})});
+$("#navSameID1,#navSameID2,#navSameID3,#navSameID4,#navSameID5,#navSameID6,#navSameID7").click(function() {
+    $("#myNav").css("width", "0%")
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%"
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%"
+}
+$("a").on("click", function(t) {
+    if ("" !== this.hash) {
+        t.preventDefault();
+        var o = this.hash;
+        $("html, body").animate({
+            scrollTop: $(o).offset().top
+        }, 800, function() {
+            window.location.hash = o
+        })
+    }
+}), AOS.init(), $('[data-spy="scroll"]').each(function() {
+    $(this).scrollspy("refresh")
+}), $(window).scroll(function() {
+    $(this).scrollTop() > 0 ? $(".header").addClass("sticky") : $(".header").removeClass("sticky")
+}), $(function() {
+    $(".portfolio-items").isotope({
+        itemSelector: ".port-img",
+        layoutMode: "fitRows"
+    }), $(".portfolio-menu ul li").click(function() {
+        $(".portfolio-menu ul li").removeClass("active"), $(this).addClass("active");
+        var t = $(this).attr("data-filter");
+        return $(".portfolio-items").isotope({
+            filter: t
+        }), !1
+    })
+});
